94354c56923de101b84b3151afe803ae54a9c477
Rasik <rasikraj01@gmail.com>
2018-01-09 22:59:08 +0530

    bg script

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,48 +1,52 @@
-console.log('Dark Mode ON');
-// black
+function night() {
+   console.log('Dark Mode ON');
+   // black
 
-let div = document.getElementsByTagName('div');
+   let div = document.getElementsByTagName('div');
 
-for(elt of div){
-   elt.style['background-color'] = 'black';
+   for(elt of div){
+      elt.style['background-color'] = 'black';
+   }
+
+   let article = document.getElementsByTagName('article');
+
+   for(elt of article){
+      elt.style['background-color'] = 'black';
+   }
+
+
+
+   let article = document.getElementsByTagName('article');
+
+   for(elt of article){
+      elt.style['background-color'] = 'black';
+   }
+   // White
+
+   let h1 = document.getElementsByTagName('h1');
+
+   let p = document.getElementsByTagName('p');
+
+   for(elt of p){
+      elt.style['color'] = 'white';
+   }
+
+   for(elt of h1){
+      elt.style['color'] = 'white';
+   }
+
+   let a = document.getElementsByTagName('a');
+
+   for(elt of a){
+      elt.style['color'] = 'white';
+   }
+
+
+   let svg = document.getElementsByTagName('svg');
+
+   for(elt of svg){
+      elt.style['color'] = 'white';
+   }
+
 }
-
-let article = document.getElementsByTagName('article');
-
-for(elt of article){
-   elt.style['background-color'] = 'black';
-}
-
-
-
-let article = document.getElementsByTagName('article');
-
-for(elt of article){
-   elt.style['background-color'] = 'black';
-}
-// White
-
-let h1 = document.getElementsByTagName('h1');
-
-let p = document.getElementsByTagName('p');
-
-for(elt of p){
-   elt.style['color'] = 'white';
-}
-
-for(elt of h1){
-   elt.style['color'] = 'white';
-}
-
-let a = document.getElementsByTagName('a');
-
-for(elt of a){
-   elt.style['color'] = 'white';
-}
-
-
-let svg = document.getElementsByTagName('svg');
-
-for(elt of svg){
-   elt.style['color'] = 'white';
-}
+   night();
