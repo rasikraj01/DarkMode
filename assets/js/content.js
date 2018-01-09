4178b163ae81e0d71edcb1b54fbae673794fcb99
Rasik <rasikraj01@gmail.com>
2018-01-09 22:02:17 +0530

    blacks and whites

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,6 +1,48 @@
 console.log('Dark Mode ON');
-let para = document.getElementsByTagName('div');
+// black
 
-for(elt of para){
+let div = document.getElementsByTagName('div');
+
+for(elt of div){
    elt.style['background-color'] = 'black';
+}
+
+let article = document.getElementsByTagName('article');
+
+for(elt of article){
+   elt.style['background-color'] = 'black';
+}
+
+
+
+let article = document.getElementsByTagName('article');
+
+for(elt of article){
+   elt.style['background-color'] = 'black';
+}
+// White
+
+let h1 = document.getElementsByTagName('h1');
+
+let p = document.getElementsByTagName('p');
+
+for(elt of p){
+   elt.style['color'] = 'white';
+}
+
+for(elt of h1){
+   elt.style['color'] = 'white';
+}
+
+let a = document.getElementsByTagName('a');
+
+for(elt of a){
+   elt.style['color'] = 'white';
+}
+
+
+let svg = document.getElementsByTagName('svg');
+
+for(elt of svg){
+   elt.style['color'] = 'white';
 }
